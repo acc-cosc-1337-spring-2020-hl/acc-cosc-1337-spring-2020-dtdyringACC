e708c8f5cea7f210bba35bb6437fd9772e781763
David Dyring <daviddyring@gmail.com>
2020-01-27 19:06:21 -0600

    explored vars example+making functions esp. "void"

--- src/examples/01_module/02_variables/main.cpp
+++ src/examples/01_module/02_variables/main.cpp
@@ -1,6 +1,18 @@
+#include "vars.h"
 
 int main() 
 {
+	display_number(10);
+
+	int num = 5;
+
+	cout << square_number(num) << "\n";
+
+	cout << num;
+
+	int num1 = square_number(5);
+
+	display_number(num1);
 
 	return 0;
 }
--- src/examples/01_module/02_variables/vars.cpp
+++ src/examples/01_module/02_variables/vars.cpp
@@ -1,1 +1,14 @@
+#include "vars.h"
 //cpp
+
+void display_number(int num1)
+{
+	cout << num1 << "\n";
+}
+
+int square_number(int num)
+{
+	//num = 100; //changes parameter values while inside function
+	
+	return num*num;
+}
